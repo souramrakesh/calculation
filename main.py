a5ccb1b5f56f36da5064885fed6027f3bf7afa5b
souramrakesh <57832253+souramrakesh@users.noreply.github.com>
2021-05-31 17:45:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ def predict():
         second_number = request.form['Please Enter the second Number']
         if first_number and second_number in "1234567890":
             predict = (int(first_number)+int(second_number))
-            return render_template('index1.html', text ="Addition of {} and {} is :{}".format(first_number,second_number,predict))
+            return render_template('templates/index1.html', text ="Addition of {} and {} is :{}".format(first_number,second_number,predict))
         else:
-            return render_template('index1.html',text ="Please Enter an integer")
+            return render_template('templates/index1.html',text ="Please Enter an integer")
 
 if __name__=="__main__":
     app.run(debug=True)
